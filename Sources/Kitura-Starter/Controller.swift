83b8f88bccfd7c612a999d611e65049a253c3810
Ricardo Balduino <balduino@us.ibm.com>
2016-10-20 14:16:25 -0500

    test

--- Sources/Kitura-Starter/Controller.swift
+++ Sources/Kitura-Starter/Controller.swift
@@ -78,8 +78,7 @@ public class Controller {
     	request.addValue("d95b7289-f8b2-43e9-a7c4-da48294b64f1", forHTTPHeaderField: "X-IBM-Client-Id")
     	
         // Excute HTTP Request
-        let session = URLSession(configuration: URLSessionConfiguration.default)
-        let task = session.dataTask(with: request as URLRequest) {
+        let task = URLSession.shared().dataTask(with: request) {
             data, response, error in
             
             // Check for error
