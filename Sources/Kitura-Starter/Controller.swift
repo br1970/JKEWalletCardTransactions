dbb8e815e482f73b9cabe4c3e81fe39a2f3f8a11
Ricardo Balduino <balduino@us.ibm.com>
2016-10-20 13:04:54 -0500

    test

--- Sources/Kitura-Starter/Controller.swift
+++ Sources/Kitura-Starter/Controller.swift
@@ -73,13 +73,13 @@ public class Controller {
     	let scriptUrl = "https://api.us.apiconnect.ibmcloud.com/balduinousibmcom-development/runSale"
         let urlWithParams = scriptUrl + "?cardNumber=\(cardNumber)&cardExp=\(cardExp)&cardCode=\(cardCode)&transAmount=\(transAmount)&transDescription=\(transDescription)&transInvoiceNumber=\(transInvoiceNumber)"
         let myUrl = NSURL(string: urlWithParams)        
-        let request = NSMutableURLRequest(URL:myUrl!)
+        let request = NSMutableURLRequest(url:myUrl!)
        	request.HTTPMethod = "POST"
     	request.addValue("d95b7289-f8b2-43e9-a7c4-da48294b64f1", forHTTPHeaderField: "X-IBM-Client-Id")
     	
  
         // Excute HTTP Request
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+        let task = URLSession.sharedSession().dataTaskWithRequest(request) {
             data, response, error in
             
             // Check for error
@@ -113,11 +113,11 @@ public class Controller {
 */            
         }
         
-        task.resume()
+       task.resume()
+       try response.status(.OK).send(responseString).end()
        	
        	//try response.status(.OK).send("{\"cardNumber\":\"\(cardNumber)\", \"cardExp\":\"\(cardExp)\", \"cardCode\":\"\(cardCode)\", \"transAmount\":\"\(transAmount)\", \"transDescription\": \"\(transDescription)\", \"transInvoiceNumber\":\"\(transInvoiceNumber)\"}").end()//      				try response.status(.OK).send("{\"cardNumber\":\"\(cardNumber)\", \"cardExp\":\"\(cardExp)\", \"cardCode\":\"\(cardCode)\", \"transAmount\":\"\(transAmount)\", \"transDescription\": \"\(transDescription)\", \"transInvoiceNumber\":\"\(transInvoiceNumber)\"}").end()       				
-       	try response.status(.OK).send(responseString).end()
-
+ 
 
 
     } else {
